63459aa62cff0cdf682b505d697c084575c2fef7
DeveloperKes <kevin.sanchez8117@gmail.com>
2025-03-20 01:43:18 -0500

    First commit

---
+++ src/main/java/net/kesgroom/cplt/CustomPlayerLimitTime.java
@@ -0,0 +1,17 @@
+package net.kesgroom.cplt;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CustomPlayerLimitTime implements ModInitializer {
+	public static final String MOD_ID = "custom-player-limit-time";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/kesgroom/cplt/CustomPlayerLimitTimeDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kesgroom.cplt;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomPlayerLimitTimeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
